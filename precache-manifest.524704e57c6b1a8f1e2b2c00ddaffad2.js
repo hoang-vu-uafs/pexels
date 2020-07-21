b947140c6f4a936f26258d279cc9e83ee17a7808
Hoang Vu <hoang.vu@uafs.edu>
2020-07-21 18:47:28 -0500

    Updates

---
+++ precache-manifest.524704e57c6b1a8f1e2b2c00ddaffad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41a7d86abb6224dd870e9f6aa584048",
+    "url": "/pexels/index.html"
+  },
+  {
+    "revision": "8af997a8e13c09e57ae0",
+    "url": "/pexels/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8dbaa29f2b3d6eb1c95e",
+    "url": "/pexels/static/js/2.edc024b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pexels/static/js/2.edc024b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af997a8e13c09e57ae0",
+    "url": "/pexels/static/js/main.1f7bc1d8.chunk.js"
+  },
+  {
+    "revision": "073456ea3ae3c637305f",
+    "url": "/pexels/static/js/runtime-main.586786be.js"
+  }
+]);
